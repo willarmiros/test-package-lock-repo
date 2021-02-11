6d87982119ffb7ed8c9f229fbd82815d4948f60b
William Armiros <armiros@amazon.com>
2021-02-11 16:24:32 -0600

    remove global lerna

--- scripts/changelog-update.sh
+++ scripts/changelog-update.sh
@@ -3,6 +3,6 @@
 echo "
 ## $1
 " > delete_me.txt
-lerna-changelog | sed '1,3d' >> delete_me.txt
-sed -e -i '/## Unreleased/r delete_me.txt' CHANGELOG.md
+npx lerna-changelog | sed '1,3d' >> delete_me.txt
+sed -i -e '/## Unreleased/r delete_me.txt' CHANGELOG.md
 rm delete_me.txt
