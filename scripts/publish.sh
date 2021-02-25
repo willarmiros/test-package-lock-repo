9a0aa7b2da1694070994e991f000a075fdab5806
William Armiros <armiros@amazon.com>
2021-02-25 12:25:10 -0800

    add npm publishing

---
+++ scripts/publish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for path in $(cat lerna.json | jq '.packages[]'); do
+  base=$(sed 's/"//g' <<< $path)  # Remove quotes
+  for package in $base; do
+    if [ -d $package ]; then
+      echo Publishing to NPM: $package
+      # pushd packages/$dir
+      # npm publish
+      # popd
+    fi
+  done
+done
